170e2180ffdae80c4176c09e7cf9840b436189ba
Danny Gaudet <danny.gaudet@isg.ca>
2012-04-15 21:02:15 -0400

    Ajout du package model pour pouvoir puller

---
+++ ca/uqam/casinotopia/model/ModelServeurClient.java
@@ -0,0 +1,35 @@
+package ca.uqam.casinotopia.model;
+
+
+import ca.uqam.casinotopia.Utilisateur;
+import ca.uqam.casinotopia.modele.Model;
+
+public class ModelServeurClient implements Model {
+
+	public int number = 0;
+	private Utilisateur utilisateur = new Utilisateur();
+	
+	public ModelServeurClient() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	/**
+	 * @return the utilisateur
+	 */
+	public Utilisateur getUtilisateur() {
+		return utilisateur;
+	}
+
+
+	/**
+	 * @param utilisateur the utilisateur to set
+	 */
+	public void setUtilisateur(Utilisateur utilisateur) {
+		this.utilisateur = utilisateur;
+	}
+
+	
+	
+	
+}
---
+++ ca/uqam/casinotopia/model/ModelServeurPrincipal.java
@@ -0,0 +1,8 @@
+package ca.uqam.casinotopia.model;
+
+import ca.uqam.casinotopia.modele.Model;
+
+
+public class ModelServeurPrincipal implements Model {
+
+}
