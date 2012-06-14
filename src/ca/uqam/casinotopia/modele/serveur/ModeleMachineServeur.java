750b9089af72bd6c26435986fd4afc0d9e674122
olivier <olivier.despres-bourgon@isg.ca>
2012-06-14 12:47:41 -0400

    ajout de la classe partie dans ModeleMachineServeur.java

--- src/ca/uqam/casinotopia/modele/serveur/ModeleMachineServeur.java
+++ src/ca/uqam/casinotopia/modele/serveur/ModeleMachineServeur.java
@@ -1,12 +1,21 @@
 package ca.uqam.casinotopia.modele.serveur;
 
+import ca.uqam.casinotopia.Jeu;
+import ca.uqam.casinotopia.Partie;
 import ca.uqam.casinotopia.modele.Modele;
+import ca.uqam.casinotopia.objet.PartieClient;
+import ca.uqam.casinotopia.objet.Utilisateur;
 import ca.uqam.casinotopia.observateur.Observable;
 import ca.uqam.casinotopia.observateur.Observateur;
 import ca.uqam.casinotopia.type.modif.TypeModif;
 
-//TODO extends Partie
-public class ModeleMachineServeur implements Modele, Observable {
+
+public class ModeleMachineServeur extends Partie implements Modele, Observable {
+
+	public ModeleMachineServeur(int id, Jeu infoJeu) {
+		super(id, infoJeu);
+		// TODO Auto-generated constructor stub
+	}
 
 	private static final long serialVersionUID = 8854771788955204109L;
 
@@ -36,6 +45,24 @@ public class ModeleMachineServeur implements Modele, Observable {
 
 	@Override
 	public TypeModif getTypeModif() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void connecter(Utilisateur utilisateur) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deconnecter(Utilisateur utilisateur) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public PartieClient creerModeleClient() {
 		// TODO Auto-generated method stub
 		return null;
 	}
